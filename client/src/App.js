66d4c6f3160cd3d3847e5fb2dcf00f097df617da
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-28 15:00:11 +0530

    Final

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import{Routes,Route} from 'react-router-dom'
 import HomePage from './pages/home/HomePage';
+import Roompage from './pages/room/Roompage';
 
 function App() {
   return (
     <div className="App">
      <Routes>
       <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/room/:roomId' element={<h1>room</h1>}></Route>
+      <Route path='/room/:roomId' element={<Roompage/>}></Route>
      </Routes>
     </div>
   );
--- client/src/pages/room/Roompage.jsx
+++ client/src/pages/room/Roompage.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
+import { useParams } from 'react-router-dom';
 
 const Roompage = () => {
+const {roomId} = useParams();
+
+const myMeeting = async(element) =>{
+    const appID = 2129250070;
+    const serverSecret = "23d23885ca0fb3658d73c31f5bfe18d3";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID , serverSecret , roomId , Date.now().toString(),'Mahi');
+    const zp = ZegoUIKitPrebuilt.create(kitToken)
+    zp.joinRoom({
+        container : element,
+        scenario : {
+            mode : ZegoUIKitPrebuilt.VideoConference
+        }
+    })
+}
   return (
     <div>
-      
+     <div ref = {myMeeting}/>
     </div>
   )
 }
